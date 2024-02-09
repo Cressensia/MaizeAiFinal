b62180f644af53127b11fb15fb826f075249b52c
Cressensia <118552106+Cressensia@users.noreply.github.com>
2024-02-09 21:20:54 +0800

    Add files via upload

--- Backend/MaizeAi/urls.py
+++ Backend/MaizeAi/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-# from .views import upload_image
 from . import views
 
+app_name = "maizeai"
+
 urlpatterns = [
-    path('image_upload_view/', views.image_upload_view, name='image_upload_view'),
+    path("upload-image/", views.image_upload_view, name="upload_image"),
+    path("image_upload_view/", views.image_upload_view, name="upload_image"),
+    path("upload_imageMaizeDisease/", views.upload_imageMaizeDisease, name="upload_imageMaizeDisease"),
+    path("get_user_results/", views.get_user_results, name="get_user_results"),
 ]
